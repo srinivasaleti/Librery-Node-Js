d43e62e1867a321867fe0b35251f5426c070954a
Aleti Srinivasarao <alti@Administrators-MacBook-Pro-2.local>
2019-07-18 17:09:07 +0530

    Add a post end point

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
+app.use(bodyParser());
 
 app.get("/", (req, res) => {
   res.send("hello");
 });
 
+app.post("/books", (req, res) => {
+  const data = req.body;
+  res.send(data);
+});
 app.listen(4000, () => {
   console.log("Starting Application");
 });
